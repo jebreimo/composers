756f09e24d4bca16dac9cbedbd12d582d49002b6
Jan Erik Breimo <jebreimo@gmail.com>
2024-07-26 21:16:40 +0200

    Further simplifications.

---
+++ src/ComposerTable.tsx
@@ -0,0 +1,291 @@
+import * as React from 'react';
+// import {alpha} from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TablePagination from '@mui/material/TablePagination';
+import TableRow from '@mui/material/TableRow';
+import TableSortLabel from '@mui/material/TableSortLabel';
+// import Toolbar from '@mui/material/Toolbar';
+// import Typography from '@mui/material/Typography';
+import Paper from '@mui/material/Paper';
+// import IconButton from '@mui/material/IconButton';
+// import Tooltip from '@mui/material/Tooltip';
+// import FormControlLabel from '@mui/material/FormControlLabel';
+// import Switch from '@mui/material/Switch';
+// import DeleteIcon from '@mui/icons-material/Delete';
+// import FilterListIcon from '@mui/icons-material/FilterList';
+import {visuallyHidden} from '@mui/utils';
+// import TextField from "@mui/material/TextField";
+import Box from '@mui/material/Box'; // Box must be imported last to avoid a bug in the MUI library (theme initialization).
+import {getDefaultComposerDb, IComposer} from "./FindComposers.ts";
+
+const composerDb = await getDefaultComposerDb();
+
+function descendingComparator<T>(a: T, b: T, orderBy: keyof T) {
+    if (b[orderBy] < a[orderBy]) {
+        return -1;
+    }
+    if (b[orderBy] > a[orderBy]) {
+        return 1;
+    }
+    return 0;
+}
+
+type Order = 'asc' | 'desc';
+
+function getComparator<Key extends keyof never>(
+    order: Order,
+    orderBy: Key,
+): (
+    a: { [key in Key]: number | string },
+    b: { [key in Key]: number | string },
+) => number {
+    return order === 'desc'
+        ? (a, b) => descendingComparator(a, b, orderBy)
+        : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+interface HeadCell {
+    disablePadding: boolean;
+    id: keyof IComposer;
+    label: string;
+    numeric: boolean;
+}
+
+const headCells: readonly HeadCell[] = [
+    {
+        id: 'surname',
+        numeric: false,
+        disablePadding: true,
+        label: 'Surname',
+    },
+    {
+        id: 'givenName',
+        numeric: false,
+        disablePadding: true,
+        label: 'Given name',
+    },
+    {
+        id: 'country',
+        numeric: false,
+        disablePadding: true,
+        label: 'Nationality',
+    },
+    {
+        id: 'birth',
+        numeric: true,
+        disablePadding: false,
+        label: 'Born',
+    },
+    {
+        id: 'death',
+        numeric: true,
+        disablePadding: false,
+        label: 'Died',
+    },
+    {
+        id: 'note',
+        numeric: false,
+        disablePadding: true,
+        label: 'Note',
+    },
+];
+
+interface EnhancedTableProps {
+    onRequestSort: (event: React.MouseEvent<unknown>, property: keyof IComposer) => void;
+    order: Order;
+    orderBy: string;
+    rowCount: number;
+}
+
+function EnhancedTableHead(props: EnhancedTableProps) {
+    const {
+        order,
+        orderBy,
+        // rowCount,
+        onRequestSort
+    } = props;
+    const createSortHandler =
+        (property: keyof IComposer) => (event: React.MouseEvent<unknown>) => {
+            onRequestSort(event, property);
+        };
+
+    return (
+        <TableHead>
+            <TableRow>
+                {headCells.map((headCell) => (
+                    <TableCell
+                        key={headCell.id}
+                        align={headCell.numeric ? 'right' : 'left'}
+                        padding={headCell.disablePadding ? 'none' : 'normal'}
+                        sortDirection={orderBy === headCell.id ? order : false}
+                    >
+                        <TableSortLabel
+                            active={orderBy === headCell.id}
+                            direction={orderBy === headCell.id ? order : 'asc'}
+                            onClick={createSortHandler(headCell.id)}
+                        >
+                            {headCell.label}
+                            {orderBy === headCell.id ? (
+                                <Box component="span" sx={visuallyHidden}>
+                                    {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                                </Box>
+                            ) : null}
+                        </TableSortLabel>
+                    </TableCell>
+                ))}
+            </TableRow>
+        </TableHead>
+    );
+}
+
+// function EnhancedTableToolbar() {
+//
+//     return (
+//         <Toolbar
+//             sx={{
+//                 pl: {sm: 2},
+//                 pr: {xs: 1, sm: 1},
+//             }}
+//         >
+//             <Typography
+//                 sx={{flex: '1 1 100%'}}
+//                 variant="h6"
+//                 id="tableTitle"
+//                 component="div"
+//             >
+//                 Composers
+//             </Typography>
+//             <TextField
+//                 id="filled-search"
+//                 label="Given name"
+//                 type="search"
+//                 variant="standard"
+//                 // onChange={(e) => setNextQuery(e.target.value)}
+//                 // onKeyDown={handleEnterPress}
+//             />
+//             <TextField
+//                 id="filled-search"
+//                 label="Given name"
+//                 type="search"
+//                 variant="standard"
+//                 // onChange={(e) => setNextQuery(e.target.value)}
+//                 // onKeyDown={handleEnterPress}
+//             />
+//         </Toolbar>
+//     );
+// }
+
+export default function EnhancedTable() {
+    const [order, setOrder] = React.useState<Order>('asc');
+    const [orderBy, setOrderBy] = React.useState<keyof IComposer>('surname');
+    const [page, setPage] = React.useState(0);
+    const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+    const handleRequestSort = (
+        // @ts-expect-error - event unused
+        event: React.MouseEvent<unknown>,
+        property: keyof IComposer,
+    ) => {
+        const isAsc = orderBy === property && order === 'asc';
+        setOrder(isAsc ? 'desc' : 'asc');
+        setOrderBy(property);
+    };
+
+    // @ts-expect-error - event unused
+    const handleChangePage = (event: unknown, newPage: number) => {
+        setPage(newPage);
+    };
+
+    const handleChangeRowsPerPage = (event: React.ChangeEvent<HTMLInputElement>) => {
+        setRowsPerPage(parseInt(event.target.value, 10));
+        setPage(0);
+    };
+
+    // Avoid a layout jump when reaching the last page with empty rows.
+    const emptyRows =
+        page > 0 ? Math.max(0, (1 + page) * rowsPerPage - composerDb.composers.length) : 0;
+
+    const visibleRows = React.useMemo(
+        () =>
+            composerDb.composers.slice().sort(getComparator(order, orderBy)).slice(
+                page * rowsPerPage,
+                page * rowsPerPage + rowsPerPage,
+            ),
+        [order, orderBy, page, rowsPerPage],
+    );
+
+    return (
+        <Box sx={{width: '100%'}}>
+            <Paper sx={{width: '100%', mb: 2}}>
+                {/*<EnhancedTableToolbar/>*/}
+                <TableContainer>
+                    <Table
+                        sx={{minWidth: 750}}
+                        aria-labelledby="tableTitle"
+                        size={'small'}
+                    >
+                        <EnhancedTableHead
+                            order={order}
+                            orderBy={orderBy}
+                            onRequestSort={handleRequestSort}
+                            rowCount={composerDb.composers.length}
+                        />
+                        <TableBody>
+                            {visibleRows.map((row, index) => {
+                                const labelId = `enhanced-table-checkbox-${index}`;
+
+                                return (
+                                    <TableRow
+                                        hover
+                                        role="checkbox"
+                                        aria-checked={false}
+                                        tabIndex={-1}
+                                        key={row.id}
+                                        selected={false}
+                                        sx={{cursor: 'pointer'}}
+                                    >
+                                        <TableCell
+                                            component="th"
+                                            id={labelId}
+                                            scope="row"
+                                            padding="none"
+                                        >
+                                            {row.surname}
+                                        </TableCell>
+                                        <TableCell>{row.givenName}</TableCell>
+                                        <TableCell>{row.country}</TableCell>
+                                        <TableCell align="right">{row.birth}</TableCell>
+                                        <TableCell align="right">{row.death}</TableCell>
+                                        <TableCell>{row.note}</TableCell>
+                                    </TableRow>
+                                );
+                            })}
+                            {emptyRows > 0 && (
+                                <TableRow
+                                    style={{
+                                        height: 33 * emptyRows,
+                                    }}
+                                >
+                                    <TableCell colSpan={6}/>
+                                </TableRow>
+                            )}
+                        </TableBody>
+                    </Table>
+                </TableContainer>
+                <TablePagination
+                    rowsPerPageOptions={[5, 10, 25]}
+                    component="div"
+                    count={composerDb.composers.length}
+                    rowsPerPage={rowsPerPage}
+                    page={page}
+                    onPageChange={handleChangePage}
+                    onRowsPerPageChange={handleChangeRowsPerPage}
+                />
+            </Paper>
+        </Box>
+    );
+}
