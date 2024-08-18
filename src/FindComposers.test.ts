c3f477ee799055966e7d7b46aa06ae601cb0e475
Jan Erik Breimo <jebreimo@gmail.com>
2024-08-18 23:21:29 +0200

    Add the first test for running a query.

--- src/FindComposers.test.ts
+++ src/FindComposers.test.ts
@@ -11,6 +11,14 @@ ai3YVsHrvFQwjQYfNc6U5VhqJd6+NCmIYra/FGhuMRh6fxvM4803dfdHkYHt04cqugJEObl3\
 test('unpacking the composers BLOB', async () => {
     const db = await unpackComposerDb(fourComposers);
     expect(db.composers.length).toEqual(4);
-    expect(db.composers[1].givenName).toEqual('Bror Axel Lille Bror');
+    expect(db.composers[1].givenName).toEqual('Bror Axel "Lille Bror"');
     expect(db.composers[3].country).toEqual('Norge, Sverige');
+});
+
+test('find one of the composers', async () => {
+    const db = await unpackComposerDb(fourComposers);
+    const composers = await db.find({expression: ".{2}j", queryType: "given", partialMatch: false});
+    expect(composers.length).toEqual(1);
+    expect(composers[0].givenName).toEqual('Maj');
+    expect(composers[0].surname).toEqual('Sønstevold');
 });
