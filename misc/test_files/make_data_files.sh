44d0d554177dd63c414cacea8cd167e557bb584a
Jan Erik Breimo <jebreimo@gmail.com>
2025-03-16 19:58:30 +0100

    Make the script that updates the database do more.

--- misc/test_files/make_data_files.sh
+++ misc/test_files/make_data_files.sh
@@ -2,7 +2,13 @@
 
 SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
+echo "Copying composers.txt from ComposerDB"
+cp -f /Users/janeb/Repositories/Misc/ComposerDB/composers.txt "$SCRIPT_DIR/all_composers.txt"
+
 for FILE in "$SCRIPT_DIR"/*.txt
 do
   python3 "$SCRIPT_DIR"/pack_composers.py "$FILE" > "$FILE.ts"
 done
+
+echo "Copying all_composers.txt.ts to src"
+cp "$SCRIPT_DIR/all_composers.txt.ts" "$SCRIPT_DIR/../../src/all_composers.txt.ts"
